3178256a7025f0e5959c6d0508c593f99ecb8fe2
KJWHUB <126055193+KJWHUB@users.noreply.github.com>
2024-01-09 18:14:26 +0900

    변수 테스트

--- vue-query-test/src/views/HomeView.vue
+++ vue-query-test/src/views/HomeView.vue
@@ -1,5 +1,21 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import style from '@/assets/variables.module.scss'
+
+const val = style
+
+console.log('tes::::::::', style)
+</script>
 
 <template>
-  <main>home</main>
+  <main>
+    home
+    <div
+      v-for="([key, value], i) in Object.entries(val)"
+      :key="i"
+      style="margin: 1rem; border: 1px solid red"
+    >
+      <p>key : {{ key }}</p>
+      <p>value : {{ value }}</p>
+    </div>
+  </main>
 </template>
